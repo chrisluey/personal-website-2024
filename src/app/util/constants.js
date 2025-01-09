d6b3c72d80261104a80bcb5e9608a9c56cfeb848
Christopher Luey <christopherluey@gmail.com>
2025-01-09 01:08:07 -0800

    resume and linkedin link update

--- src/app/util/constants.js
+++ src/app/util/constants.js
@@ -1,5 +1,5 @@
 const RESUME_LINK = "/Resume_Chris_Luey_2024.pdf"
 const GITHUB_LINK = "https://github.com/chrisluey"
-const LINKEDIN_LINK = "https://www.linkedin.com/in/christopher-luey-834941195/"
+const LINKEDIN_LINK = "https://www.linkedin.com/in/chrisluey98/"
 
 export { RESUME_LINK, GITHUB_LINK, LINKEDIN_LINK };
