ad17594fce9627dc52387812cd780aa31f45ad6d
Christopher Luey <christopherluey@gmail.com>
2025-04-04 16:51:36 -0700

    hotfix for removed imports

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,13 @@
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+import AboutSection from "./components/AboutSection";
+import ProjectsSection from "./components/ProjectsSection";
+import EmailSection from "./components/EmailSection";
+import Footer from "./components/Footer";
+import AchievementsSection from "./components/AchievementsSection";
+import ScrollArrows from "./components/ScrollArrows";
+
+
 export default function Home() {
   return (
     <main className="flex flex-col bg-gradient-to-br from-blue-500 to-purple-500">
